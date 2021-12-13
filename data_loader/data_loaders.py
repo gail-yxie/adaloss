1446bd28d967d965a44b41887d271252b066a123
Yuege (Gail) Xie <yuege@ices.utexas.edu>
2021-12-12 22:22:17 -0600

    Remove trans_type

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -9,10 +9,8 @@ class CIFAR100DataLoader(BaseDataLoader):
                  num_workers=4, pin_memory=True):
         config = ConfigParser.get_instance()
         cfg_trainer = config['trainer']
-        trans_type = cfg_trainer['trans_type']
-        transform_train, transform_val = {}, {}
 
-        transform_train['vit'] = transforms.Compose([
+        transform_train = transforms.Compose([
             # transforms.ColorJitter(brightness= 0.4, contrast= 0.4, saturation= 0.4, hue= 0.1),
             transforms.Resize(256),
             transforms.RandomCrop(224),
@@ -20,21 +18,9 @@ class CIFAR100DataLoader(BaseDataLoader):
             transforms.ToTensor(),
             transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
         ])
-        transform_val['vit'] = transforms.Compose([
+        transform_val = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
-        ])
-
-        transform_train['res'] = transforms.Compose([
-            # transforms.ColorJitter(brightness= 0.4, contrast= 0.4, saturation= 0.4, hue= 0.1),
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
-        ])
-        transform_val['res'] = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
         ])
@@ -45,8 +31,8 @@ class CIFAR100DataLoader(BaseDataLoader):
 
         self.train_dataset, self.val_dataset = get_cifar100(config['data_loader']['args']['data_dir'], cfg_trainer,
                                                             train=training,
-                                                            transform_train=transform_train[trans_type],
-                                                            transform_val=transform_val[trans_type])
+                                                            transform_train=transform_train,
+                                                            transform_val=transform_val)
 
         super().__init__(self.train_dataset, batch_size, shuffle, validation_split, num_workers, pin_memory,
                          val_dataset=self.val_dataset)
